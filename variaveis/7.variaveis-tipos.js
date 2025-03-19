5dd9ad3442d5ea09d0be30ac59f9aa6b66470d44
LeonSantana7 <leon.santana854@gmail.com>
2025-03-19 16:43:13 -0300

    Estudos 19/03

---
+++ variaveis/7.variaveis-tipos.js
@@ -0,0 +1,13 @@
+let nome = "Leon"; //string
+let idade = 30; //number / int
+let altura = 1.73; //number / float
+
+
+let nota; // undefined
+console.log(nota);
+
+nota = 10;
+console.log(nota);
+
+nota = null; // null
+console.log(nota);
---
+++ variaveis/9.variaveis-concatenacao.js
@@ -0,0 +1,6 @@
+let nome = "Leon";
+let sobrenome = "Santana";
+
+let nomeCompleto = nome + sobrenome;
+
+console.log(nomeCompleto);
