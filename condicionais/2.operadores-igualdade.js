494e48ecd21441d667c8d01cd9648e024439af7e
LEON SANTANA BARBOSA <021.884631@senaicimatec.edu.br>
2025-03-24 17:01:20 -0300

    Estudos

---
+++ condicionais/2.operadores-igualdade.js
@@ -0,0 +1,6 @@
+let x = 10;
+
+const ehIgual =  x == 18;
+const ehDiferente = x != 18;
+console.log(ehIgual);
+console.log(ehDiferente);
---
+++ condicionais/3.operadores-identidade.js
@@ -0,0 +1,4 @@
+const ehIdentico = 18 === "18";
+const ehOpostoIdentico = 18 !== "18";
+console.log(ehIdentico);
+console.log(ehOpostoIdentico);
