602d1e3cef597a386bc0e93c771ada0760c8310d
LeonSantana7 <leon.santana854@gmail.com>
2025-03-31 15:35:26 -0300

    For-de-c

---
+++ repeticao/2.for-de-c.js
@@ -0,0 +1,20 @@
+const nomesDeEstudantes = [
+    'Jose',
+    'Joao',
+    'Maria',
+    'joana',
+    'alice',
+    'andreia',
+];
+
+// let indice = 0;
+// // enquanto
+// while (indice < nomesDeEstudantes.length) {
+//     console.log(nomesDeEstudantes[indice]);
+//     indice++;
+// }
+
+for(let i = 0;i < nomesDeEstudantes.length;i++){
+    console.log(nomesDeEstudantes[i]);
+}
+
