da5cedb92b4f70940aba1bf382aaf94d890cff64
LeonSantana7 <leon.santana854@gmail.com>
2025-04-01 17:01:55 -0300

    Estudos

---
+++ objetos/1.intro.js
@@ -0,0 +1,15 @@
+// Objeto
+const pessoa = {
+    nome: 'Leon',
+    idade: 27,
+    altura: 1.82,
+    peso: 80,
+};
+
+pessoa.nome = "João";
+pessoa['peso'] = 85;
+console.log(`Meu nome é ${pessoa.nome}, tenho ${pessoa.idade} anos e peso ${pessoa.peso} kg`);
+
+
+
+
